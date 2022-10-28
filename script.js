6e49918b0fd798578af371b2d3339a4d03acdf18
jejem8430 <jer.munoz30@gmail.com>
2022-10-28 10:17:30 +0200

    final commit

--- script.js
+++ script.js
@@ -3,30 +3,18 @@
 
 const message = "Merci pour votre message nous vous répondrons dans les plus brefs délais.";
 let contact = document.querySelector("#contactForm")
-
-console.log(contact);
-
-
 contact.addEventListener("submit", function (event) {
         event.preventDefault();
-        console.log("log");
-
     alert(message);
-    });
-
-const moveNav = document.querySelectorAll(".li");
-const query = document.querySelector(".frites")
-query.addEventListener("mouseover", () => {
-    console.log("coca")
-})
-console.log(moveNav)
-for (const itemNav of moveNav) {
+});
+    
+// mouseNav 
+const moveNav = document.querySelectorAll(".navli");
+   for (const itemNav of moveNav) {
     itemNav.addEventListener(`mouseover`, function () {
-        console.log("mouseOver")
         itemNav.classList.add("navJS");
     });
     itemNav.addEventListener(`mouseleave`, function () {
-        console.log('mouseLeave')
         itemNav.classList.remove("navJS");
     });
 }
